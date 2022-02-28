63713156bc718be99c03e9a219518e750c0aa09d
nabilmersni <nabilmersni123@gmail.com>
2022-02-28 17:09:33 +0100

    Adding splash screen

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QSplashScreen>
+#include <QTimer>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
+
+    QSplashScreen *splash = new QSplashScreen;
+    splash->setPixmap(QPixmap(":/resource/img/splash.jpg"));
+    splash->show();
+    QTimer::singleShot(1500,splash,&QWidget::close);
+    QTimer::singleShot(1500,&w,SLOT(show()));
+
+
     return a.exec();
 }
