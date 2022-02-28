c24c968ec6cd0aff7c517036c0d57aca19790b6c
nabilmersni <nabilmersni123@gmail.com>
2022-02-28 17:26:38 +0100

    Connect the app with the DB

--- main.cpp
+++ main.cpp
@@ -3,17 +3,35 @@
 #include <QApplication>
 #include <QSplashScreen>
 #include <QTimer>
+#include "connection.h"
+#include <QMessageBox>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
 
-    QSplashScreen *splash = new QSplashScreen;
-    splash->setPixmap(QPixmap(":/resource/img/splash.jpg"));
-    splash->show();
-    QTimer::singleShot(1500,splash,&QWidget::close);
-    QTimer::singleShot(1500,&w,SLOT(show()));
+    //specific declaration
+    //specific declaration
+
+    Connection c;
+    bool test=c.createconnect();
+
+    if(!test)
+    {
+        QMessageBox::critical(nullptr, QObject::tr("database is not open"),
+                    QObject::tr("connection failed.\n"
+                                "Click Cancel to exit."), QMessageBox::Cancel);
+
+    }else {
+        //specific declaration
+        //specific declaration
+        QSplashScreen *splash = new QSplashScreen;
+        splash->setPixmap(QPixmap(":/resource/img/splash.jpg"));
+        splash->show();
+        QTimer::singleShot(1500,splash,&QWidget::close);
+        QTimer::singleShot(1500,&w,SLOT(show()));
+    }
 
 
     return a.exec();
