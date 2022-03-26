1aa8b9f8db73e5220f307008345aecc10faa0f65
Ali <braiekali123@gmail.com>
2022-03-26 23:29:02 +0100

    SMTP

---
+++ smtp.cpp
@@ -0,0 +1,75 @@
+#include "smtp.h"
+
+
+
+Smtp::Smtp( const QString &user, const QString &pass, const QString &host, int port, int timeout )
+{
+    socket = new QSslSocket(this);
+
+    connect(socket, SIGNAL(readyRead()), this, SLOT(readyRead()));
+    connect(socket, SIGNAL(connected()), this, SLOT(connected() ) );
+    connect(socket, SIGNAL(error(QAbstractSocket::SocketError)), this,SLOT(errorReceived(QAbstractSocket::SocketError)));
+    connect(socket, SIGNAL(stateChanged(QAbstractSocket::SocketState)), this, SLOT(stateChanged(QAbstractSocket::SocketState)));
+    connect(socket, SIGNAL(disconnected()), this,SLOT(disconnected()));
+
+
+    this->user = user;
+    this->pass = pass;
+
+    this->host = host;
+    this->port = port;
+    this->timeout = timeout;
+
+
+}
+
+void Smtp::sendMail(const QString &from, const QString &to, const QString &subject, const QString &body)
+{
+    message = "To: " + to + "\n";
+    message.append("From: " + from + "\n");
+    message.append("Subject: " + subject + "\n");
+    message.append(body);
+    message.replace( QString::fromLatin1( "\n" ), QString::fromLatin1( "\r\n" ) );
+    message.replace( QString::fromLatin1( "\r\n.\r\n" ),
+    QString::fromLatin1( "\r\n..\r\n" ) );
+    this->from = from;
+    rcpt = to;
+    state = Init;
+    socket->connectToHostEncrypted(host, port); //"smtp.gmail.com" and 465 for gmail TLS
+    if (!socket->waitForConnected(timeout)) {
+         qDebug() << socket->errorString();
+     }
+
+    t = new QTextStream( socket );
+
+
+
+}
+
+Smtp::~Smtp()
+{
+    delete t;
+    delete socket;
+}
+void Smtp::stateChanged(QAbstractSocket::SocketState socketState)
+{
+
+    qDebug() <<"stateChanged " << socketState;
+}
+
+void Smtp::errorReceived(QAbstractSocket::SocketError socketError)
+{
+    qDebug() << "error " <<socketError;
+}
+
+void Smtp::disconnected()
+{
+
+    qDebug() <<"disconneted";
+    qDebug() << "error "  << socket->errorString();
+}
+
+void Smtp::connected()
+{
+    qDebug() << "Connected ";
+}
